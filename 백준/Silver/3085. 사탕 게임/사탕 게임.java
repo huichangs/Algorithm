29502befb840f9d06deef9abfabe5944eee4dfe2
YunHuiChang <102274136+huichangs@users.noreply.github.com>
2023-12-10 17:37:01 +0900

    [Silver II] Title: 사탕 게임, Time: 228 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.java
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.java
@@ -3,15 +3,13 @@ import java.util.*;
 
 public class Main {
     static int n;
-    static StringBuilder sb = new StringBuilder();
+    static int ans = 0;
     static int[] dx = {0, 0, -1, 1};
     static int[] dy = {-1, 1, 0, 0};
     static char[][] map = new char[55][55];
-    static int ans = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        map = new char[n][n];
         for(int i=0; i<n; i++) {
             String str = br.readLine();
             for(int j=0; j<n; j++) {
