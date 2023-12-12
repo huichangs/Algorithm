532e7d9f79e876a90163df029f54054b74a9767a
YunHuiChang <102274136+huichangs@users.noreply.github.com>
2023-12-12 23:44:41 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 696 ms, Memory: 52004 KB -BaekjoonHub

---
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++){
+            int v = Integer.parseInt(st.nextToken());
+            arr[i] = arr[i - 1] + v;
+        }
+
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            bw.write(String.valueOf(arr[end] - arr[start - 1]));
+            bw.write("\n");
+        }
+
+        bw.close();
+    }
+}
