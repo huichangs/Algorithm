1d5409ebe88287aaf7ef5047e28eff6b28f9a029
YunHuiChang <102274136+huichangs@users.noreply.github.com>
2025-01-21 23:11:27 +0900

    Time: 1663 ms (5.43%), Space: 79.5 MB (16.74%) - LeetHub

---
+++ LeetCode/Medium/2017-grid-game/2017-grid-game.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        int size = grid[0].size();
+        if(size == 1){
+            return 0;
+        }
+        if(size == 2){
+            return min(grid[0][1], grid[1][0]);
+        }
+        vector<vector<long long>> arr(2, vector<long long>(size, 0));
+
+        arr[1][0] = grid[1][0];
+        arr[0][size - 1] = grid[0][size - 1];
+
+        for(int i = 1; i < size; i++){
+            int j = size - i - 1;
+            arr[1][i] = arr[1][i - 1] + grid[1][i];
+            arr[0][j] = arr[0][j + 1] + grid[0][j];
+        }
+
+        arr[1][size - 1] = 0;
+        arr[0][0] = 0;
+
+        for(int i = 0; i < 2; i++){
+            for(int j = 0; j < size; j++){
+                cout << arr[i][j] << " ";
+            }
+            cout << endl;
+        }
+
+        long long answer = arr[0][1];
+        for(int i = 2; i < size; i++){
+            long long tmp = max(arr[0][i], arr[1][i - 2]);
+
+            answer = min(tmp, answer);
+        }
+        answer = min(answer, arr[1][size - 2]);
+
+        return answer;
+    }
+};
