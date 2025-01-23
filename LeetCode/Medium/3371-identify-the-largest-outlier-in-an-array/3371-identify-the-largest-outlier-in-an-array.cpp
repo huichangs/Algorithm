e862dbc68b9a3e5376662827ee7081856dfb1483
YunHuiChang <102274136+huichangs@users.noreply.github.com>
2025-01-23 14:11:57 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ LeetCode/Medium/3371-identify-the-largest-outlier-in-an-array/3371-identify-the-largest-outlier-in-an-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int getLargestOutlier(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+
+        int answer = INT_MIN;
+        int sum = 0;
+
+        for(int i : nums){
+            sum += i;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+            int specialSum = nums[i];
+            int outlier = sum - (2 * specialSum);
+            cout << "specialSum " << specialSum << " outlier: " << outlier << endl;
+            
+            if(binary_search(nums.begin(), nums.begin() + i, outlier)){
+                answer = max(answer, outlier);
+            }
+            if(binary_search(nums.begin() + i + 1, nums.end(), outlier)){
+                answer = max(answer, outlier);
+            }
+        }
+
+        return answer;
+    }
+};
