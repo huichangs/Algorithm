2eca0a35c3aa8671aff0d0211798d6f8f1b428fd
YunHuiChang <102274136+huichangs@users.noreply.github.com>
2025-02-21 12:36:21 +0900

    Time: 11 ms (22.39%), Space: 18 MB (33.28%) - LeetHub

---
+++ LeetCode/Medium/0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        map<int, int> hashmap;
+        int maxV = 0;
+
+        for(int i = 0; i < nums.size(); i++){
+            hashmap[nums[i]]++;
+            maxV = max(maxV, nums[i]);
+        }
+
+        vector<int> dp(maxV + 1, 0);
+        dp[1] = 1 * hashmap[1];
+
+        for(int i = 2; i <= maxV; i++){
+            dp[i] = max(dp[i - 1], dp[i - 2] + (i * hashmap[i]));
+        }
+
+        return dp[maxV];
+    }
+};
